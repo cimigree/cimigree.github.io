be75267ad9f420845c1838af403b02a7989c594a
Cindy Green <cimigreen@hotmail.com>
2017-04-17 12:32:20 -0400

    Displaying blog from Wordpress API.

--- mysite.js
+++ mysite.js
@@ -1,5 +1,18 @@
 
+Handlebars.registerHelper('formatDate', function (date, format) {
+    var mmnt = moment(date);
+    return mmnt.format(format);
+});
 
-api:
 
-https://public-api.wordpress.com/rest/v1.1/sites/cimigreen.wordpress.com/posts/
+$().ready(function () {
+         $.getJSON("https://public-api.wordpress.com/rest/v1.1/sites/cimigreen.wordpress.com/posts/", function(result) {   
+        var source = $("#entry-template").html();
+        var template = Handlebars.compile(source);
+        var html = template(result);
+        $(".post").html(html);
+        $(".post .blog-content img").addClass("col-md-3 text-left");
+            $(".post p img:first-child + p").addClass("col-offset-2 col-md-7 text-right");
+    });
+    });
+
