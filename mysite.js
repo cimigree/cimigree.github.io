bb7b0bde29186f8c0791292a27b5e28343405807
Cindy Green <cimigreen@hotmail.com>
2017-04-23 23:09:36 -0400

    Added slight changes based on mentor's suggestions.

--- mysite.js
+++ mysite.js
@@ -11,8 +11,34 @@ $().ready(function () {
         var template = Handlebars.compile(source);
         var html = template(result);
         $(".post").html(html);
-        $(".post .blog-content img").addClass("col-md-3 text-left");
-            $(".post p img:first-child + p").addClass("col-offset-2 col-md-7 text-right");
+         $(".post .blog-content img").addClass("col-md-3 text-left");
+          $(".post .blog-content img p").addClass("col-offset-2 col-md-7 text-right");
     });
     });
 
+// function add_class_after_img() {
+// 	var was_img_found = false;
+// 	$("#blogpost p").each(function() {
+// 		if (!was_img_found) {
+// 			was_img_found = ($(this).find("img").length > 0);
+// 			return;
+// 		}
+// 		$(this).addClass("col-offset-2 col-md-7 text-right");
+// 	})
+// }
+// add_class_after_img();
+
+$().ready(function add_class_after_img() {
+	var was_img_found = false;
+	$("#blogpost p").each(function() {
+		if (!was_img_found) {
+			if ($(this).find("img").length > 0) {
+				was_img_found = true;
+			}
+			return;
+		}
+		if (was_img_found) {
+			$(this).addClass("col-offset-2 col-md-7 text-right");
+		}
+	});
+});
